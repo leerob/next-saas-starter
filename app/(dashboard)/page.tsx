17939581f43b012193c495ecc2cff830016bf662
Lee Robinson <me@leerob.io>
2024-09-16 17:18:22 -0500

    Update URL

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
               </p>
               <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
                 <a
-                  href="https://github.com/leerob/next-saas-starter"
+                  href="https://vercel.com/templates/next.js/next-js-saas-starter"
                   target="_blank"
                 >
                   <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-lg px-8 py-4 inline-flex items-center justify-center">
@@ -108,11 +108,11 @@ export default function HomePage() {
             </div>
             <div className="mt-8 lg:mt-0 flex justify-center lg:justify-end">
               <a
-                href="https://github.com/leerob/next-saas-starter"
+                href="https://vercel.com/templates/next.js/next-js-saas-starter"
                 target="_blank"
               >
                 <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-xl px-12 py-6 inline-flex items-center justify-center">
-                  Deploy your own
+                  View the code
                   <ArrowRight className="ml-3 h-6 w-6" />
                 </Button>
               </a>
