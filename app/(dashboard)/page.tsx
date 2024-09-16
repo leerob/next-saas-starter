f146d6988e26600b2da3aa9c622095d1baaed328
Lee Robinson <me@leerob.io>
2024-09-16 17:21:18 -0500

    Update link

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -108,7 +108,7 @@ export default function HomePage() {
             </div>
             <div className="mt-8 lg:mt-0 flex justify-center lg:justify-end">
               <a
-                href="https://vercel.com/templates/next.js/next-js-saas-starter"
+                href="https://github.com/leerob/next-saas-starter"
                 target="_blank"
               >
                 <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-xl px-12 py-6 inline-flex items-center justify-center">
