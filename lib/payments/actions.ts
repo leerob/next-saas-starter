215501f3f9e5a7abd36114c88bc176af6fc47104
Ghulam Mudassir <48262881+Mudassir1234@users.noreply.github.com>
2024-09-18 00:58:47 +0100

    Update actions.ts

--- lib/payments/actions.ts
+++ lib/payments/actions.ts
@@ -7,7 +7,7 @@ import { withTeam } from '@/lib/auth/middleware';
 export const checkoutAction = withTeam(async (formData, team) => {
   const priceId = formData.get('priceId') as string;
   await createCheckoutSession({ team: team, priceId });
-});
+});;
 
 export const customerPortalAction = withTeam(async (_, team) => {
   const portalSession = await createCustomerPortalSession(team);
