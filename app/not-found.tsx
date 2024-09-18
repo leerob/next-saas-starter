c3203ccdaf346d946e24142bdde080b6e2eacc2e
Lee Robinson <me@leerob.io>
2024-09-18 12:54:54 -0500

    Update 404 page design

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,23 @@
-import Link from "next/link";
-import { CircleIcon } from "lucide-react";
+import Link from 'next/link';
+import { CircleIcon } from 'lucide-react';
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="max-w-lg space-y-8 p-4 text-center">
+    <div className="flex items-center justify-center min-h-[100dvh]">
+      <div className="max-w-md space-y-8 p-4 text-center">
         <div className="flex justify-center">
           <CircleIcon className="size-12 text-orange-500" />
         </div>
         <h1 className="text-4xl font-bold text-gray-900 tracking-tight">
-          404
-          <span className="block text-orange-500">Page Not Found</span>
+          Page Not Found
         </h1>
         <p className="text-base text-gray-500">
           The page you are looking for might have been removed, had its name
-          changed or is temporarily unavailable.
+          changed, or is temporarily unavailable.
         </p>
         <Link
           href="/"
-          className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+          className="max-w-48 mx-auto flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
         >
           Back to Home
         </Link>
