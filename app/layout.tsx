2c6cdfc04321d84fce03bfa3feee920d5444ad18
Jose M Molina <jose.molina@icims.com>
2024-10-01 13:08:08 +0200

    feat(layout): check user preference server side

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
+import { LOCAL_STORAGE_KEY } from '@/components/dark-mode-toggle'
 
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
@@ -27,6 +28,21 @@ export default function RootLayout({
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
+      <head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (localStorage.getItem(${LOCAL_STORAGE_KEY}) === 'dark' || (!(${LOCAL_STORAGE_KEY} in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                  document.documentElement.style.setProperty('color-scheme', 'dark');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                  document.documentElement.style.removeProperty('color-scheme');
+                }
+              `,
+            }}
+        />
+      </head>
       <body className="min-h-[100dvh] bg-gray-50">
         <UserProvider userPromise={userPromise}>{children}</UserProvider>
       </body>
