b44a3f1f2bc690526c8324283b8ae34055c9e450
Raphaël Badia <1901628+raphaelbadia@users.noreply.github.com>
2025-01-30 11:10:45 +0100

    Update terminal.tsx

--- app/(dashboard)/terminal.tsx
+++ app/(dashboard)/terminal.tsx
@@ -7,7 +7,7 @@ export function Terminal() {
   const [terminalStep, setTerminalStep] = useState(0);
   const [copied, setCopied] = useState(false);
   const terminalSteps = [
-    'git clone https://github.com/leerob/next-saas-starter',
+    'git clone https://github.com/nextjs/saas-starter',
     'pnpm install',
     'pnpm db:setup',
     'pnpm db:migrate',
